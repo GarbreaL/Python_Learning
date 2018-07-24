1f6b3b45e974c517fdb74cdcaaa047f097988039
GarbreaL <33507858+GarbreaL@users.noreply.github.com>
2018-07-24 22:59:41 +0800

    函数的定义和调用

---
+++ lesson3_function.py
@@ -0,0 +1,24 @@
+#定义函数
+def MyFirstFunction():
+    '函数说明是在这里'
+    print("This is MyFirstFunction")
+
+#调用函数
+MyFirstFunction()    
+#函数说明查看
+print("函数说明查看方式：MyFirstFunction.__doc__")
+MyFirstFunction.__doc__
+
+
+#多参数
+def MySecondFunction(name,age):
+    print(name +'年龄是:'+age)
+#参数可以通过形参名配合实参，不需要考虑顺序
+MySecondFunction(age='18',name='校长')
+
+#参数的默认值
+def MyThirdFunction(name='校长',age='20'):
+    print(name +'年龄是:'+age)
+
+#没有传递参数的，会使用默认参数
+MyThirdFunction(age="50")    
