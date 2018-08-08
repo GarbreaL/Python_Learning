012fb4620439929e6ed682b86a512f596935ce5e
GarbreaL <33507858+GarbreaL@users.noreply.github.com>
2018-08-08 08:42:42 +0800

    再提交一次

--- lesson6_finacy.py
+++ lesson6_finacy.py
@@ -1,6 +1,6 @@
-mGramNow=input("请输入贵金属当前质量：")
+mGramNow=input("请输入贵金属当前质量（克）：")
 mCost=input("请输入当前成本价格：")
 mPrice=input("请输入当前价格：")
 mTargetCost=input("请输入目标成本价格：")
 mGramx=(mGramNow*mCost-mTargetCost*mGramNow)/(mTargetCost-mPrice)
-print("当前价格下，要达到目标成本需要购买 {0}，需要花费{1}", mGramx, mGramx * mPrice)
+print("当前价格下，要达到目标成本需要购买贵金属{0}克，需要花费{1}", mGramx, mGramx * mPrice)
