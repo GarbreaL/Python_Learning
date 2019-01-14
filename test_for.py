e7d4d71f884ddacfcce163db44633c70dbf35439
GarbreaL <33507858+GarbreaL@users.noreply.github.com>
2019-01-14 09:28:42 +0800

    增加for循环和while循环

--- test_for.py
+++ test_for.py
@@ -9,3 +9,12 @@ for i in range(0, 10):
 print('range(0,10,1)')
 for i in range(0, 10,1):
     print(i, end='\n')
+
+for i in range(10):
+    j = i+1
+    print(j, end='\n')
+    
+while (i < 10):
+    j = i + 1
+    print(j, end='\n')
+    i=i+1
