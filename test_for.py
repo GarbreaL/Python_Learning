603c016decb863e1b471f9e5748014bb569c7a76
GarbreaL <33507858+GarbreaL@users.noreply.github.com>
2019-01-14 10:09:37 +0800

    while

--- test_for.py
+++ test_for.py
@@ -10,11 +10,14 @@ print('range(0,10,1)')
 for i in range(0, 10,1):
     print(i, end='\n')
 
+print('for 1=>10')
 for i in range(10):
     j = i+1
     print(j, end='\n')
-    
+
+print('while<10')
+i=1
 while (i < 10):
-    j = i + 1
+    j = i 
     print(j, end='\n')
     i=i+1
