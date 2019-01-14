82dd9e67da4000de490d02bbf82906f7ed6ff266
GarbreaL <33507858+GarbreaL@users.noreply.github.com>
2019-01-14 09:23:13 +0800

    新增文件TEST_FOR，测试range函数

---
+++ test_for.py
@@ -0,0 +1,11 @@
+print('range(10)')
+for i in range(10):
+    print(i, end='\n')
+
+print('range(0,10)')
+for i in range(0, 10):
+    print(i, end='\n')
+
+print('range(0,10,1)')
+for i in range(0, 10,1):
+    print(i, end='\n')
