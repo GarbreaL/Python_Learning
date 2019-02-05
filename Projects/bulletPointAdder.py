89219efb259ba5158d51a6dc3307482500f87cc7
GarbreaL <33507858+GarbreaL@users.noreply.github.com>
2019-02-05 16:03:48 +0800

    add projects files

--- Projects/bulletPointAdder.py
+++ Projects/bulletPointAdder.py
@@ -2,7 +2,11 @@
 # bulletPointAdder.py 获取剪贴版上的数据，在每行数据前加上"* ",再返回给剪贴板
 
 import pyperclip
+
 text = pyperclip.paste()
-
-
-pyperclip.copy()
+lines = text.split('\n')
+for i in range(len(lines)):
+    lines[i] = '* '+lines[i]
+text = '\n'.join(lines)
+pyperclip.copy(text)
+print('数据调整完成，请进行粘贴操作！')
